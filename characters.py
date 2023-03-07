5b0f06d22d82df2998f6083f4ea40ee38bc27ca5
neenzk61 <alisonqual1990@gmail.com>
2023-03-07 01:28:26 -0500

    Made changes so the sloth is ready to fight the boss

--- characters.py
+++ characters.py
@@ -1,15 +1,23 @@
 class maincharacter:
-    def __init__(self, name, hitpoints):
+    def __init__(self, name, hitpoints, damage):
         self.name = name
         self.hitpoints = hitpoints
+        self.damage = damage
 
     def attack_with_claws(self):
         return 24
+    
+    def attack_with_torch(self):
+        return 100
+    
+
         
 
-def create_character(name):
-    player = maincharacter(name, 100)
+def create_character(name, hitpoints, damage):
+    player = maincharacter(name, hitpoints, damage)
     print("Welcome " + player.name)
     return player
+
     
+
 

1c572061b8d999fbf4ae6685bc02375408ef2e9a
neenzk61 <alisonqual1990@gmail.com>
2023-03-09 03:44:00 -0500

    Introducded super functions to my code and also added a simple print function for the list of items for potions

--- inventory_potions_weapons.py
+++ inventory_potions_weapons.py
@@ -1,21 +1,11 @@
-class inventory:
-    def __init__(self):
-        self.item = []
-
-    def add_potions(self, potions):
-        self.item.append(potions)
-    
-    
-    def use_item(self, item):
-        if isinstance(item, potion):
-            item.use(sloth)
-            
-
-class potion:
-    def __init__(self, name, effect):
+class item:
+    def __init__(self, name):
         self.name = name
-        self.effect = effect
 
+class potion(item):
+    def __init__(self, name, effect):
+        super().__init__(name)
+        self.effect = effect
 
     def apply(self, sloth):
         if self.effect == "small_potion":
@@ -23,7 +13,37 @@ class potion:
         if self.effect == "medium_potion":
             sloth.heal(100)
         if self.effect == "small_elixer":
-            sloth.maxhitpoints += 50
+            sloth.max_hp += 50
+
+class weapon:
+    def __init__(self, item, effect):
+        self.item = item
+        self.effect = effect
+
+    def equip(self, sloth):
+        if self.equip == "small_torch":
+            sloth.damage = 50
+        if self.equip == "sloth_claws":
+            sloth.damage = 24
+
+class inventory:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        if isinstance(item, (potion, weapon)):
+            self.items.append(item)
+
+    def potion_list(self):
+        potion_l = [p for p in self.items if isinstance(p, potion)]
+        if len(potion_l) == 0:
+            print("There are currently no potions in the inventory")
+        else:
+            print("List of potions:")
+        for p in potion_l:
+            print("- " + p.name)
+
+
 
 potions = {
     "small_potion": potion("small potion", "small_potion"),
@@ -31,6 +51,7 @@ potions = {
     "small_elixer": potion("small elixer of life", "small_elixer"),
 }
 
-
-
-
+weapons = {
+    "small_torch": weapon("torch", "small_torch"),
+    "sloth_claws": weapon("claws", "sloth_claws"),
+}
--- sloth.py
+++ sloth.py
@@ -5,13 +5,6 @@ class mainsloth:
         self.hp = hp
         self.damage = damage
 
-    def attack_with_claws(self):
-        return 24
-    
-    def attack_with_torch(self):
-        return 100
-    
-
         
 
 def create_sloth(name, maxhp, hp, damage):
